3116e548a7a9adaa05e39c08a2cfdfc9229bdbe2
Anshul <anshulbhaskar50@gmail.com>
2026-03-08 23:48:54 +0530

    DB schema

---
+++ init-db.sql
@@ -0,0 +1,8 @@
+-- Create databases for each microservice
+CREATE DATABASE merchant_db;
+CREATE DATABASE payment_db;
+CREATE DATABASE bank_db;
+CREATE DATABASE transaction_db;
+CREATE DATABASE fraud_db;
+CREATE DATABASE settlement_db;
+CREATE DATABASE auth_db;
