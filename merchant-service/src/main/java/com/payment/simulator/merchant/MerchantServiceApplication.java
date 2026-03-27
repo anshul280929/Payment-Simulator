4739443a1b224211879ef2a163a1e4cfa42405b8
Anshul <anshulbhaskar50@gmail.com>
2026-03-27 23:58:46 +0530

    Solved local PG issue

--- merchant-service/src/main/java/com/payment/simulator/merchant/MerchantServiceApplication.java
+++ merchant-service/src/main/java/com/payment/simulator/merchant/MerchantServiceApplication.java
@@ -1,13 +1,26 @@
 package com.payment.simulator.merchant;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class MerchantServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MerchantServiceApplication.class, args);
+    }
+
+    @Bean
+    Runnable logDatasourceConfig(
+            @Value("${spring.datasource.url:}") String datasourceUrl,
+            @Value("${spring.datasource.username:}") String datasourceUsername
+    ) {
+        return () -> System.out.println(
+                "[merchant-service] Effective spring.datasource.url=" + datasourceUrl
+                        + ", username=" + datasourceUsername
+        );
     }
 }
